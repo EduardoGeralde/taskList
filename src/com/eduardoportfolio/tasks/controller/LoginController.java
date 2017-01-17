8193d476853777126080c7ad1f368fcc5176c2ed
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-17 09:59:51 -0200

    controller to login

---
+++ src/com/eduardoportfolio/tasks/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.eduardoportfolio.tasks.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eduardoportfolio.tasks.dao.JdbcUserDao;
+import com.eduardoportfolio.tasks.model.User;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping ("loginForm")
+	public String loginForm(){
+		return "login-form";
+	}
+	
+	@RequestMapping("makeLogin")
+	public String makeLogin(User user, HttpSession session){
+		
+		if(new JdbcUserDao().userExists(user)) {
+			session.setAttribute("userLogged", user);
+			return "menu";
+		}
+		return "redirect:loginForm";
+	}
+}
