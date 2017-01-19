3a7fc682f55aa286114c940810f3666258e2ba56
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-19 12:55:39 -0200

    changes

--- src/com/eduardoportfolio/tasks/jpa/GenerateTables.java
+++ src/com/eduardoportfolio/tasks/jpa/GenerateTables.java
@@ -3,10 +3,18 @@ package com.eduardoportfolio.tasks.jpa;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * @author Eduardo Geralde Neto
+ * 
+ * This is a example of how generate the table manually with JPA.
+ */
+
 public class GenerateTables {
 
 	public static void main(String[] args) {
 		
+		//The Persistence class (same name of the XML) is responsible to load the XML and initialize
+		//the configurations. The result of this configurations is a EntityManagerFactory
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("Task");
 		factory.close();
 	}
